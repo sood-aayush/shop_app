332816e5b41f27ce3a4325827e2e861264e0290d
clownno1 <122744957+clownno1@users.noreply.github.com>
2023-02-03 17:57:38 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MeriDukaan',
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          accentColor: Colors.red,
+          fontFamily: 'Lato'),
+      home: ProdectsOverviewScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meri Dukaan'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/prouduct_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GestureDetector(
+        child: GridTile(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            backgroundColor: Colors.black87,
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
