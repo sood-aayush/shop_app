75b4a98c580f8bda1cc874275685f0fe39c793fa
clownno1 <122744957+clownno1@users.noreply.github.com>
2023-02-05 11:35:57 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,14 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MeriDukaan',
-      theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-          accentColor: Colors.red,
-          fontFamily: 'Lato'),
-      home: ProdectsOverviewScreen(),
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MeriDukaan',
+        theme: ThemeData(
+            primarySwatch: Colors.blueGrey,
+            accentColor: Colors.red,
+            fontFamily: 'Lato'),
+        home: ProdectsOverviewScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          ProductDetailScreen.routename: (context) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
